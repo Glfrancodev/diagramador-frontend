9bc09606ca20a47c9bd23b50f2e9afd3d8a59225
Glfrancodev <gabrielandreslopezfrancodev@gmail.com>
2025-04-29 23:37:50 -0400

    Produccion 6

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../utils/axiosInstance"; // Asegúrate de tener esta configuración de Axios
+import axiosInstance from "../services/axiosInstance"; // Asegúrate de tener esta configuración de Axios
 
 function Register() {
   const [name, setName] = useState("");
